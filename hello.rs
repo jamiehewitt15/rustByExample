db58afb2a225109e71b3fd49a3a4451658876690
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-02-02 12:17:32 +0300

    adding println examples

--- hello.rs
+++ hello.rs
@@ -10,4 +10,14 @@ fn main() {
     // Comments:
     let x = 5 +  90 + 5;
     println!("Is `x` 10 or 100? x = {}", x);
+
+    // Formatted print:
+    println!("{} days", 31i64);
+
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+
+    println!("{object} {verb} {subject}",
+             object="the smelly cat",
+             subject="the big brown fox",
+             verb="runs away from");
 }
